a3d1fabf50bd9f70b3b4ab2e8147a61709f2ea79
Hurkus <8364277+Hurkus@users.noreply.github.com>
2025-06-18 19:31:11 +0200

    Added decoding for input device capabilities.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -8,19 +9,38 @@
 
 #include "DEBUG.hpp"
 
-// https://gist.github.com/TriceHelix/de47ed38dcb4f7216b26291c47445d99
-
 using namespace std;
 using namespace std::filesystem;
 
 
-// ----------------------------------- [ Functions ] ---------------------------------------- //
+// ----------------------------------- [ Structures ] --------------------------------------- //
 
 
 struct Device {
 	filesystem::path path;
 	string name;
+	string description;
 };
+
+
+// ----------------------------------- [ Functions ] ---------------------------------------- //
+
+
+constexpr bool isHex(char c){
+	return ('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F');
+}
+
+
+constexpr uint8_t unhex(char c){
+	if ('0' <= c && c <= '9')
+		return c - '0';
+	else if ('a' <= c && c <= 'f')
+		return 10 + c - 'a';
+	else if ('A' <= c && c <= 'F')
+		return 10 + c - 'A';
+	else
+		return UINT8_MAX;
+}
 
 
 // ----------------------------------- [ Functions ] ---------------------------------------- //
@@ -46,26 +66,27 @@ static bool readFile(const filesystem::path& path, string& buff){
 }
 
 
-constexpr bool isHex(char c){
-	return ('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F');
-}
-
-constexpr uint8_t unhex(char c){
-	if ('0' <= c && c <= '9')
-		return c - '0';
-	else if ('a' <= c && c <= 'f')
-		return 10 + c - 'a';
-	else if ('A' <= c && c <= 'F')
-		return 10 + c - 'A';
+/**
+ * @brief Extract capability bit from bit vector.
+ * @param capabilites Bit field of capabilities.
+ * @param eventCode Event code defined in `linux/input-event-codes.h`. Eg: `KEY_A`.
+ */
+static bool isCapable(const vector<uint64_t>& capabilites, uint32_t eventCode){
+	const uint32_t w = eventCode / 64;
+	const uint32_t b = eventCode % 64;
+	
+	if (w < capabilites.size())
+		return (capabilites[w] & (1 << b)) != 0;
 	else
-		return UINT8_MAX;
+		return false;
 }
 
 
 // ----------------------------------- [ Functions ] ---------------------------------------- //
 
 
-static string getDeviceName(const string& name){
+static string getDeviceDescription(const string& name){
+	assert(name.length() > 0);
 	ifstream in = ifstream(path("/sys/class/input/") / name / "device/name");
 	string line;
 	getline(in, line);
@@ -73,66 +94,84 @@ static string getDeviceName(const string& name){
 }
 
 
-static bool getDeviceCapabilities(const string& name, const char* capability){
-	const filesystem::path dev_path = path("/sys/class/input/") / name / "device/capabilities/key";
+/**
+ * @brief Decode capabilites file of an input device.
+ * @note File structure: https://gist.github.com/TriceHelix/de47ed38dcb4f7216b26291c47445d99
+ * @param name Name of the device at `/sys/class/input/<name>`
+ * @param capability Capabilities file in `/sys/class/input/<name>/device/capabilities/<capability>`: `ev`, `key`, `led`, `abs`, `ff`, `msc`, `rel`, `snd`, `sw`, ...
+ * @return Bit field of capabilities. Querryable with enums from `linux/input-event-codes.h` or `isCapable(...)`.
+ */
+static vector<uint64_t> getDeviceCapabilities(const string& name, const char* capability){
+	assert(capability != nullptr);
+	assert(name.length() > 0);
+	const filesystem::path dev_path = path("/sys/class/input/") / name / "device/capabilities/" / capability;
+	
+	vector<uint64_t> bits = {};
+	uint64_t hex_group = 0;
+	int bit_count = 0;
+	
+	string data;
+	if (!readFile(dev_path, data)){
+		err:
+		ERROR("Failed to parse device capabilities of device '%s'.", dev_path.c_str());
+		bits.clear();
+		return bits;
+	}
 	
 	// Read in reverse and decode hex.
 	// Example: 120013 -> 1100'1000'0000'0000'0100'1000
 	// Example: f 2 -> 0100'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000 1111
-	
-	string data;
-	readFile(dev_path, data);
-	// data = "120013";	// DEBUG
-	// data = "f 7";	// DEBUG
-	// data = "33eff 0 0 483ffff17aff32d bfd4444600000000 1 130c730b17c007 ffbf7bfad9415fff febeffdfffefffff fffffffffffffffe";	// DEBUG
-	
-	vector<bool> bits = {};
-	
-	int b = 0;
 	for (int i = int(data.length()) - 1 ; i >= 0 ; i--){
 		const char c = data[i];
 		
-		// Ignore newline.
 		if (c == '\n'){
 			continue;
 		}
 		
-		// Pad 4*(16-b) false bits.
+		// Pad with zero bits.
 		else if (isspace(c)){
-			for (int ii = 4*(16-b) ; ii > 0 ; ii--)
-				bits.push_back(0);
-			b = 0;
+			bits.push_back(hex_group);
+			hex_group = 0;
+			bit_count = 0;
 		}
 		
-		// Append hex bits in reverse order.
+		// Append hex bits.
 		else if (isHex(c)){
-			uint8_t h = unhex(c);
-			bits.push_back((h >> 0) & 1);
-			bits.push_back((h >> 1) & 1);
-			bits.push_back((h >> 2) & 1);
-			bits.push_back((h >> 3) & 1);
-			b += 1;
+			const uint8_t hx = unhex(c);
+			
+			if (bit_count >= 64){
+				bits.push_back(hex_group);
+				hex_group = 0;
+				bit_count = 0;
+			}
+			
+			hex_group |= (uint64_t(hx) << bit_count);
+			bit_count += 4;
 		}
 		
 		else {
-			ERROR("Failed to parse device capabilities of device '%s'.", dev_path.c_str());
-			return false;
+			goto err;
 		}
 		
 	}
 	
-	cout << data << endl;
-	for (int i = 0 ; i < bits.size() ; i++){
-		cout << bits[i];
+	// Push remaining hex group.
+	if (bit_count > 0){
+		bits.push_back(hex_group);
+		hex_group = 0;
+		bit_count = 0;
 	}
-	cout << endl;
 	
+	// cout << data << endl;
+	// for (int i = 0 ; i < bits.size() ; i++){
+	// 	if (i > 0)
+	// 		cout << " ";
+	// 	for (int ii = 0 ; ii < 64 ; ii++)
+	// 		cout << ((bits[i] >> ii) & 1);
+	// }
+	// cout << endl;
 	
-	cout << "KEY_NUMLOCK: ";
-	cout << (KEY_NUMLOCK < bits.size() ? bits[KEY_NUMLOCK] : 0);
-	cout << endl;
-	
-	return true;
+	return bits;
 }
 
 
@@ -144,11 +183,10 @@ static void getEventDevices(vector<Device>& devs){
 			continue;
 		}
 		
-		const string devname = e.path().filename();
-		
 		Device& dev = devs.emplace_back();
 		dev.path = e.path();
-		dev.name = getDeviceName(devname);
+		dev.name = e.path().filename();
+		dev.description = getDeviceDescription(dev.name);
 	}
 }
 
@@ -162,22 +200,27 @@ int main(int argc, char const* const* argv){
 	
 	getEventDevices(devs);
 	
-	for (Device& dev : devs){
-		printf("%s:\n", dev.path.c_str());
-		printf("    name: %s\n", dev.name.c_str());
-	}
-	
-	// for (int i = int(devs.size()) - 1 ; i >= 0 ; i--){
-	// 	if (devs[i].name.find("eyboard") == string::npos)
-	// 		devs.erase(devs.begin() + i);
-	// }
-	
 	// for (Device& dev : devs){
 	// 	printf("%s:\n", dev.path.c_str());
 	// 	printf("    name: %s\n", dev.name.c_str());
 	// }
 	
-	// getDeviceCapabilities("event16", "key");
+	for (Device& dev : devs){
+		vector<uint64_t> cap = getDeviceCapabilities(dev.name, "key");
+		
+		if (isCapable(cap, KEY_NUMLOCK)){
+			printf("%s:\n", dev.path.c_str());
+			printf("    name: %s\n", dev.description.c_str());
+			printf("    numlock: %d\n", true);
+		}
+		
+	}
+	
+	// vector<uint64_t> cap = getDeviceCapabilities("event16", "key");
+	// cout << "KEY_NUMLOCK: ";
+	// cout << isCapable(cap, KEY_NUMLOCK);
+	// cout << endl;
+	
 	return 0;
 }
 
